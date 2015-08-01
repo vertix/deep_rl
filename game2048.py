337e3bf264a126c8acc75a4b59e1422f7b10da99
Sergey Arkhangelskiy <mc.vertix@gmail.com>
2015-08-01 12:04:35 -0700

    Step of 2048 now returns reward

--- game2048.py
+++ game2048.py
@@ -28,6 +28,7 @@ class Game2048(base.Environment):
     clean_row = row[row != 0]
     result = []
     i = 0
+    reward = 0
     while i < clean_row.size:
       if (i == clean_row.size - 1) or clean_row[i] != clean_row[i + 1]:
         # No joining happens
@@ -35,27 +36,35 @@ class Game2048(base.Environment):
         i += 1
       else:  # Two consequtive blocks join
         result.append(clean_row[i] + 1)
+        reward = 2. ** (clean_row[i] + 1)
         i += 2
 
-    return np.array(result + [0] * (4 - len(result)))
+    return np.array(result + [0] * (4 - len(result))), reward
 
 
   def ProcessAction(self, action):
       """Performs one step given selected action. Returns step reward."""
       if action < 0 or action > 3:
         return
+      reward = 0.
       if action == 0:  # up
         for i in range(4):
-          self._state[:, i] = Game2048._StackRow(self._state[:, i])
+          self._state[:, i], rew = Game2048._StackRow(self._state[:, i])
+          reward += rew
       elif action == 1:  # down
         for i in range(4):
-          self._state[::-1, i] = Game2048._StackRow(self._state[::-1, i])
+          self._state[::-1, i], rew = Game2048._StackRow(self._state[::-1, i])
+          reward += rew
       elif action == 2:  # left
         for i in range(4):
-          self._state[i, :] = Game2048._StackRow(self._state[i, :])
+          self._state[i, :], rew = Game2048._StackRow(self._state[i, :])
+          reward += rew
       elif action == 3:  # right
         for i in range(4):
-          self._state[i, ::-1] = Game2048._StackRow(self._state[i, ::-1])
+          self._state[i, ::-1], rew = Game2048._StackRow(self._state[i, ::-1])
+          reward += rew
+      else:
+        return 0.
 
       empty_cells = []
       for x in range(4):
@@ -70,7 +79,8 @@ class Game2048(base.Environment):
         self._state[cell] = random.choice([1, 1, 1, 1, 1, 1, 1, 1, 1, 2])
 
 
-      print self._state
+      print self._state, reward
+      return reward
 
 
 if __name__ == "__main__":
