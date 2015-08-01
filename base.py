d53830140e49575723c33bef7f9a82efd40f154f
Sergey Arkhangelskiy <mc.vertix@gmail.com>
2015-08-01 11:59:35 -0700

    Update tabs to 2 spaces.

--- base.py
+++ base.py
@@ -1,37 +1,37 @@
 class Environment(object):
-    """Base class for environment."""
-    def __init__(self):
-        pass
+  """Base class for environment."""
+  def __init__(self):
+    pass
 
-    @property
-    def num_of_actions(self):
-        raise NotImplementedError
+  @property
+  def num_of_actions(self):
+    raise NotImplementedError
 
-    def GetState(self):
-        """Returns current state as numpy (possibly) multidimensional array.
+  def GetState(self):
+    """Returns current state as numpy (possibly) multidimensional array.
 
-        If the current state is terminal, returns None.
-        """
-        raise NotImplementedError
+    If the current state is terminal, returns None.
+    """
+    raise NotImplementedError
 
-    def ProcessAction(self, action):
-        """Performs one step given selected action. Returns step reward."""
-        raise NotImplementedError
+  def ProcessAction(self, action):
+    """Performs one step given selected action. Returns step reward."""
+    raise NotImplementedError
 
 class Agent(object):
-    """Base class for different agents."""
-    def __init__(self):
-        pass
+  """Base class for different agents."""
+  def __init__(self):
+    pass
 
-    def ChooseAction(self, state):
-        pass
+  def ChooseAction(self, state):
+    pass
 
 def PlayEpisode(env, agent):
+  state = env.GetState()
+  total_reward = 0.
+  while state is not None:
+    action = agent.ChooseAction(state)
+    total_reward += env.ProcessAction(action)
     state = env.GetState()
-    total_reward = 0.
-    while state is not None:
-        action = agent.ChooseAction(state)
-        total_reward += env.ProcessAction(action)
-        state = env.GetState()
-        
-    return total_reward
+
+  return total_reward
--- game2048.py
+++ game2048.py
@@ -7,105 +7,105 @@ import sys
 import base
 
 class Game2048(base.Environment):
-    def __init__(self, seed=None):
-        self._state = np.zeros([4, 4])
-        # Set up the start position
-        self._state[1, 1] = 1
-        self._state[2, 2] = 1
+  def __init__(self, seed=None):
+    self._state = np.zeros([4, 4])
+    # Set up the start position
+    self._state[1, 1] = 1
+    self._state[2, 2] = 1
 
-        if seed is not None:
-            random.seed(seed)
+    if seed is not None:
+      random.seed(seed)
 
-    def num_of_actions(self):
-        return 4
+  def num_of_actions(self):
+    return 4
 
-    def GetState(self):
-        return self._state
+  def GetState(self):
+    return self._state
 
-    @staticmethod
-    def _StackRow(row):
-        """Takes the row and joins it from higher indexes to lower"""
-        clean_row = row[row != 0]
-        result = []
-        i = 0
-        while i < clean_row.size:
-            if (i == clean_row.size - 1) or clean_row[i] != clean_row[i + 1]:
-                # No joining happens
-                result.append(clean_row[i])
-                i += 1
-            else:  # Two consequtive blocks join
-                result.append(clean_row[i] + 1)
-                i += 2
+  @staticmethod
+  def _StackRow(row):
+    """Takes the row and joins it from higher indexes to lower"""
+    clean_row = row[row != 0]
+    result = []
+    i = 0
+    while i < clean_row.size:
+      if (i == clean_row.size - 1) or clean_row[i] != clean_row[i + 1]:
+        # No joining happens
+        result.append(clean_row[i])
+        i += 1
+      else:  # Two consequtive blocks join
+        result.append(clean_row[i] + 1)
+        i += 2
 
-        return np.array(result + [0] * (4 - len(result)))
-
-
-    def ProcessAction(self, action):
-        """Performs one step given selected action. Returns step reward."""
-        if action < 0 or action > 3:
-            return
-        if action == 0:  # up
-            for i in range(4):
-                self._state[:, i] = Game2048._StackRow(self._state[:, i])
-        elif action == 1:  # down
-            for i in range(4):
-                self._state[::-1, i] = Game2048._StackRow(self._state[::-1, i])
-        elif action == 2:  # left
-            for i in range(4):
-                self._state[i, :] = Game2048._StackRow(self._state[i, :])
-        elif action == 3:  # right
-            for i in range(4):
-                self._state[i, ::-1] = Game2048._StackRow(self._state[i, ::-1])
-
-        empty_cells = []
-        for x in range(4):
-            for y in range(4):
-                if self._state[x, y] == 0:
-                    empty_cells.append((x, y))
-
-        if not empty_cells:
-            self._state = None  # Terminal state
-        else:
-            cell = random.choice(empty_cells)
-            self._state[cell] = random.choice([1, 1, 1, 1, 1, 1, 1, 1, 1, 2])
+    return np.array(result + [0] * (4 - len(result)))
 
 
-        print self._state
+  def ProcessAction(self, action):
+      """Performs one step given selected action. Returns step reward."""
+      if action < 0 or action > 3:
+        return
+      if action == 0:  # up
+        for i in range(4):
+          self._state[:, i] = Game2048._StackRow(self._state[:, i])
+      elif action == 1:  # down
+        for i in range(4):
+          self._state[::-1, i] = Game2048._StackRow(self._state[::-1, i])
+      elif action == 2:  # left
+        for i in range(4):
+          self._state[i, :] = Game2048._StackRow(self._state[i, :])
+      elif action == 3:  # right
+        for i in range(4):
+          self._state[i, ::-1] = Game2048._StackRow(self._state[i, ::-1])
+
+      empty_cells = []
+      for x in range(4):
+        for y in range(4):
+          if self._state[x, y] == 0:
+            empty_cells.append((x, y))
+
+      if not empty_cells:
+        self._state = None  # Terminal state
+      else:
+        cell = random.choice(empty_cells)
+        self._state[cell] = random.choice([1, 1, 1, 1, 1, 1, 1, 1, 1, 2])
+
+
+      print self._state
 
 
 if __name__ == "__main__":
-    game = Game2048()
-    state = game.GetState()
+  game = Game2048()
+  state = game.GetState()
 
-    fig = plt.figure()
-    ax = fig.add_subplot(111)
-    cmap = colors.ListedColormap(['black', 'red', 'green', 'blue', 'yellow',
-                                  'orange', 'lime', 'white'])
-    bounds = [-0.5, 0.5, 1.5, 2.5, 3.5, 4.5, 5.5, 6.5]
-    norm = colors.BoundaryNorm(bounds, cmap.N)
+  fig = plt.figure()
+  ax = fig.add_subplot(111)
+  cmap = colors.ListedColormap(['black', 'red', 'green', 'blue', 'yellow',
+                                'orange', 'lime', 'white'])
+  bounds = [-0.5, 0.5, 1.5, 2.5, 3.5, 4.5, 5.5, 6.5]
+  norm = colors.BoundaryNorm(bounds, cmap.N)
 
-    window = ax.imshow(state, cmap=cmap, norm=norm, interpolation='none')
+  window = ax.imshow(state, cmap=cmap, norm=norm, interpolation='none')
 
-    def OnKeyPress(event, env):
-        action = 0
-        if event.key == 'up':
-            action = 0
-        elif event.key == 'down':
-            action = 1
-        elif event.key == 'left':
-            action = 2
-        elif event.key == 'right':
-            action = 3
-        elif event.key == 'q':
-            sys.exit()
-        else:
-            return
-        print 'Action %d' % action
-        env.ProcessAction(action)
-        window.set_data(env.GetState())
-        fig.canvas.draw()
+  def OnKeyPress(event, env):
+    action = 0
+    if event.key == 'up':
+      action = 0
+    elif event.key == 'down':
+      action = 1
+    elif event.key == 'left':
+      action = 2
+    elif event.key == 'right':
+      action = 3
+    elif event.key == 'q':
+      sys.exit()
+    else:
+      return
+    print 'Action %d' % action
+    env.ProcessAction(action)
+    window.set_data(env.GetState())
+    fig.canvas.draw()
 
 
-    cid = fig.canvas.mpl_connect('key_press_event',
-                                  lambda e: OnKeyPress(e, game))
-    plt.show()
+  cid = fig.canvas.mpl_connect('key_press_event',
+                                lambda e: OnKeyPress(e, game))
+  plt.show()
